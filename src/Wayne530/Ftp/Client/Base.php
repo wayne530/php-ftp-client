f098b5e87a6f723d8521a950a15d222dc1dafc25
Y. Wayne Huang <wayne530@gmail.com>
2016-06-17 11:20:04 -0700

    use __CLASS__ instead of get_class() which includes the namespace

--- src/Wayne530/Ftp/Client/Base.php
+++ src/Wayne530/Ftp/Client/Base.php
@@ -49,7 +49,7 @@ abstract class Base implements Client_Interface {
      * @return string
      */
     public function getType() {
-        return get_class($this);
+        return __CLASS__;
     }
 
 }
